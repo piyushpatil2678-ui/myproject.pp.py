d27d43b9b92f66288f8b30e340f6a997c2308773
Piyush Patil <piyushpatil2678@gmail.com>
2026-09-16 23:24:16 +0530

    build a recursion function

---
+++ function_recursion.py
@@ -0,0 +1,12 @@
+def greater_num():                   # it is logic 
+    a = int(input("enter number 1:"))
+    b = int(input("enter number 2:"))
+    if(a>b):
+     print("a is greater ")
+    else:
+     print("b is greater ")   
+
+greater_num()
+greater_num()             # it is the calling of logic ,it means us body kaa us kar rha hai 
+greater_num()
+greater_num()
